6a40473f2561b50d0f050360fa564475b4c42294
Arnab Dutta <137763219+Arnab0056@users.noreply.github.com>
2024-01-20 19:23:40 +0000

    commmit

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,45 @@
+//let score = 33
+//console.log(typeof score)
+//console.log(typeof(score))
+//let score = "33"
+//console.log(typeof score)
+//console.log(typeof(score))
+//let valueInNumber = Number(score)
+//console.log(typeof valueInNumber)
+// let score = "33abc"
+// console.log(typeof score)
+// console.log(typeof(score))
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+// let score = null
+// console.log(typeof score)
+// console.log(typeof(score))
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+// let score = true
+// console.log(typeof score)
+// console.log(typeof(score))
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+// let score = "Arnab"
+// console.log(typeof score)
+// console.log(typeof(score))
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+// Notes of conversion
+// "33" => 33
+// "33abc" => NaN
+// true =>1 ; faulse => 0
+// let isLoggedIn = 1;
+// let booleanIsLOggedIn  = Boolean(isLoggedIn)
+// console.log(booleanIsLOggedIn)
+let Arnab = 1;
+let arnabLovesSharmi = Boolean(Arnab);
+console.log(arnabLovesSharmi);
+ let Sharmi = 1;
+ let sharmiLovesArnab = Boolean(Sharmi);
+ console.log(sharmiLovesArnab);
--- README.md
+++
@@ -1,2 +0,0 @@
-# JS-Hindi-YouTube
-A code repo for chai aur Code Channel 
