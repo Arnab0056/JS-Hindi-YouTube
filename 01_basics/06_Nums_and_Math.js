4e090bef913e19b2f06b6f8bdfef9550211c8379
Arnab Dutta <137763219+Arnab0056@users.noreply.github.com>
2024-01-29 08:08:01 +0000

    save the changes

---
+++ 01_basics/06_Nums_and_Math.js
@@ -0,0 +1,44 @@
+//  const ss = 4000;
+//  console.log(ss);
+
+// const balance = new Number(400);
+// console.log(balance);
+
+// console.log(balance.toString()) //method to convert into string
+// console.log(typeof balance);
+
+// console.log(balance.toString().length); //propert to cheque length
+// console.log(balance.toFixed(3));
+
+// const otherNumber = 23.8966
+// console.log(otherNumber.toPrecision(3)); // method to make number as per given in toPrecision here 3.
+
+// const value = 59888.65
+// console.log(value.toPrecision(5));
+
+// const hundreds = 100000
+// console.log(hundreds.toLocaleString("en-In"));
+
+// console.log(Math.abs(-4)); // method to make negetive value into positive 
+// console.log(Math.round(4.4));  // method to make a number in round format
+// console.log(Math.ceil(4.2)); // method to make to its heigher version
+// console.log(Math.floor(4.9)); // method to make to its lover version
+
+// console.log(Math.min(4,3,6,9)); // method to find the lowest Number
+// console.log(Math.max(9,6,8,7)); // method to find the heigher number
+
+// console.log(Math.random()); // It gives a random value from 0 to 1. 
+// console.log((Math.random()*10) +1);
+// console.log(Math.floor(Math.random()*10) +1); // to round of this number
+
+// const min = 1;
+// const max = 6;
+// console.log(Math.floor(Math.random()* (max - min + 1)) + min) // very important *****
+
+// const min = 15
+// const max = 30
+// console.log(Math.floor(Math.random()* (max- min + 1)) + min)
+
+const min = 50
+const max = 60
+console.log(Math.floor(Math.random()* (max - min + 1) + min))
