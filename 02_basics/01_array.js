f2639bf604e53aa3dfed4676caa3b5b1d3537aa0
Arnab Dutta <137763219+Arnab0056@users.noreply.github.com>
2024-01-30 13:46:36 +0000

    save the changes

---
+++ 02_basics/01_array.js
@@ -0,0 +1,71 @@
+//   const myArray = [0, 1, 2, 3, 4, 5]
+//   const myHeros= ["Saktiman", "Ironman", "Batman"]
+//   const myArr2 = new Array(1, 2, 3, 4)
+
+// //   console.log(myArray[1]);  // find position value where 1 is situated.
+
+
+// //   //Array methods
+// //   myArray.push(6)     // to add value in array, value added in the last of the array  
+// //   myArray.push(7)     
+// //   console.log(myArray);
+
+// // myArray.pop()         //  to remove last value of the array 
+// // console.log(myArray)
+
+// // myArray.unshift(9);
+// // console.log(myArray)   // add value in the front of the array.
+
+// // myArray.shift();
+// // console.log(myArray)   // remove one value from the front 
+
+// // console.log(myArray.includes(99));  //  for finding value in array
+// // console.log(myArray.indexOf(4));    // for finding index number 
+// // console.log(myArray.indexOf(0));
+
+
+// // const newArr = myArray.join();     // .join converts array value into string 
+// // console.log(myArray) 
+// // console.log(newArr);
+
+// // SLICE , SPLICE operation
+
+// // console.log("A", myArray);
+// // const myArr1 = myArray.slice(1, 3)
+// // console.log(myArr1)
+// // console.log("B", myArray)
+// //another examole of slice
+// let arr3 = [56,67,23,89,59,78];
+// const myArr3 = arr3.slice(1, 4)
+// console.log(myArr3);
+
+// const myArr4 = myArr3.splice(1, 3)
+// console.log(myArr4)
+
+let nArr = [0, 1, 2, 3, 4, 5]
+console.log("A", nArr)
+const myArr5 = nArr.slice(1,3);
+console.log(myArr5)
+console.log("B", nArr)
+const myArr6 = nArr.splice(1, 3)
+console.log(nArr)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
