88308ff4a5f7ac28e798d539f460687b196d6cf2
Arnab Dutta <137763219+Arnab0056@users.noreply.github.com>
2024-01-30 05:04:23 +0000

    save the changes

---
+++ 01_basics/07_Time_and_Date.js
@@ -0,0 +1,14 @@
+let myDate = new Date();
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toDateString())
+// console.log(myDate.toJSON())
+// console.log(myDate.toISOString())
+
+// let myCreatedDate = new Date(2023, 0, 1, 5, 3)
+// console.log(myCreatedDate.toLocaleString());
+
+// let myCreatedDate = new Date("2023-1-5")   // yy-mm-dd
+let myCreatedDate = new Date("02-15-2023");
+console.log(myCreatedDate.toDateString());
