15fa383ecfc477c0a5cf165ac84cbe71fda30fc1
Arnab Dutta <137763219+Arnab0056@users.noreply.github.com>
2024-02-02 16:41:33 +0000

    SAve The change

---
+++ 03_basics/03_scopes.js
@@ -0,0 +1,61 @@
+// // NESTED SCOPES
+
+// function one(){
+//     const userName = "Arnab"
+    
+
+//     function two() {
+//         const website = "YOU-TUBE"
+//         console.log(userName)
+//         }
+//         // console.log(website);
+//         two();
+// }
+
+// one();
+
+//NESTED SCOPES in IF AND ELSE
+
+
+// if(true){
+//     const userName = "Arnab Dutta"
+//     if (userName==="Arnab Dutta") {
+//         const website = " YouTube"
+//         console.log(userName + website);
+        
+    // }
+    // console.log(website);
+    
+// }
+// console.log(userName);
+
+
+
+
+
+// +++++++++++INTERESTING++++++++++
+
+
+console.log(addOne(5));
+function addOne(numb) {
+    return numb+1
+}
+
+
+
+
+// const addTwo = function(numb){
+//     return numb + 2
+// }
+// console.log(addTwo(10));
+
+
+////////+++++++++++++++++++++++++
+
+
+
+
+console.log(addTwo(10))
+const addTwo = function(numb){
+    return numb + 2
+}
