b2ac2a19abbac25a86ad5ca2ca70fdf7fdd51664
Arnab Dutta <137763219+Arnab0056@users.noreply.github.com>
2024-02-02 13:32:55 +0000

    Save the change

---
+++ 03_basics/01_function.js
@@ -0,0 +1,87 @@
+// function sayMyname (){
+//     console.log("A");
+//     console.log("r");
+//     console.log("n");
+//     console.log("a");
+//     console.log("b");
+
+// }
+// // sayMyname()
+
+// function addTwoNumbers(number1, number2) {
+//     console.log(number1+number2);
+// }
+// // addTwoNumbers(3,4);
+
+// // another process to store value in variable.
+
+// function addTwoNumbers(number1, number2) {
+//     console.log(number1+number2);
+// }
+// // const result = addTwoNumbers(3,4);
+// // console.log("Result: ", result); // But here we can't find result 
+
+// //Another method to declare a function-------
+// function addTwoNumbers(number1, number2) {
+//     let result = number1 + number2;
+//     return result;
+//     console.log("arnab")  // This code not executed because nothing is executed after return command.
+//  }
+// //  const result = addTwoNumbers(3,4);
+// //  console.log("Result: ",result)
+
+//  //Another Simple Process------
+//  function addTwoNumbers(number1, number2) {
+//     return number1 + number2
+
+//  }
+//  console.log(addTwoNumbers(3,4));
+
+
+// Many process to pass the value in arguement and here is an example that how to show a welcome message for user.
+
+// function logInUserMessage(username){
+//     return `${username} is just logged in`
+    
+// }
+// console.log(logInUserMessage("Arnab"))
+// console.log(logInUserMessage()) // if there is no value in arguement 
+
+
+
+//here comes the concept of if
+// function logInUserMessage(username){
+//     if(username === undefined){
+//         console.log("Please enter a username");
+//         return
+//     }
+//     return `${username} is just logged in`
+    
+// }
+// console.log(logInUserMessage())
+
+// ANother symbol
+// function logInUserMessage(username){
+//     if(!username){
+//         console.log("Please enter a username");
+//         return
+//     }
+//     return `${username} is just logged in`
+    
+// }
+// console.log(logInUserMessage())
+
+// //ANother concept
+// function logInUserMessage(username = "sam"){
+//     if(!username){
+//         console.log("Please enter a username");
+//         return
+//     }
+//     return `${username} is just logged in`
+    
+// }
+// console.log(logInUserMessage("Arnab"))
+
+
+
+ 
