5d831a001b52df605171fa638c6813854143d769
Arnab Dutta <137763219+Arnab0056@users.noreply.github.com>
2024-02-08 18:20:50 +0000

    sdfgf

---
+++ 05_iteration/one.js
@@ -0,0 +1,17 @@
+// // for (let i = 0; i < 10; i++) { 
+// //     console.log(i);
+// // }
+// for (let i = 0; i <= 10; i++) { 
+//      const element = i;
+//      if (element== 5) {
+//         console.log("5 is the best number");
+//      }
+//      console.log(element);
+//      }
+
+for (let i = 1; i <= 10; i++) {
+    for (let j = 1; j <= 10; j++) {
+        console.log(`Inner Loop value ${j} and outer loop value ${i}`);
+        }
+    
+}
