97dd37a61a5152aafe708033a55292059f779cd7
Arnab Dutta <137763219+Arnab0056@users.noreply.github.com>
2024-02-08 16:55:49 +0000

    Save the changes

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,45 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// // }
+// const month = "march"
+
+// switch (month) {
+//     case "january":
+//         console.log("January");
+//         break;
+//     case "feb":
+//         console.log("feb");
+//         break;
+//     case "march":
+//         console.log("march");
+//         break;
+//     case "april":
+//         console.log("april");
+//         break;
+
+//     default:
+//         console.log("default case match");
+//         break;
+// }
+
+const month = 3
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("February");
+        break;
+    case 3:
+        console.log("March");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,30 @@
+// const userEmail = []
+// if (userEmail) {
+//     console.log("Got User Email");
+// }else {
+//     console.log("Don't have user Email");
+// }
+
+
+// if (userEmail.length === 0) {
+//     console.log("Array is empty");
+// }
+
+// const emptyObj = {}
+
+// if (Object.keys(emptyObj).length===0) {
+
+// console.log("object is empty");
+// }
+// Nullish Coalescing Operator (??): null undefined 
+let val1
+val1 = undefined ?? 15 
+// val1 = null ?? 10
+// val1 = 10 ?? 15
+
+// console.log(val1);
+
+
+// TERNARY OPERATOR
+let score = 100
+score < 20 ? console.log("score less than 20") : console.log("score more than 20");
