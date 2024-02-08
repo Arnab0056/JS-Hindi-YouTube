6ee97535f6442c71eccbd77a58f321319c7c6a03
Arnab Dutta <137763219+Arnab0056@users.noreply.github.com>
2024-02-08 16:07:30 +0000

    save the changes

---
+++ 04_control_flow/one.js
@@ -0,0 +1,45 @@
+// let temparature = 41
+
+// if (temparature >= 40 ) {
+//     console.log("temparature is more than 40");
+    
+// } else {
+//     "temparature is less than 40"
+// }
+
+// let score = 200;
+// if (score > 100) {
+//     let power = "fly"
+//     console.log(` user power : ${power}`);
+    
+// }
+
+// let score2 = 1000
+
+// if (score2 < 500) {
+//     console.log("less than 500");
+//     }else if (score2 < 600) {
+//         console.log("less than 600");
+        
+//     }else if (score2 < 700){
+//         console.log("less than 700");
+//     }else if (score2 < 800) {
+//         console.log("less than 800");
+//     }else if (score2 < 900) {
+//         console.log("less than 900"); 
+//     }else {console.log("yipee");
+//     }
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if (userLoggedIn && debitCard) {
+    console.log("Allow to buy course")
+    
+}
+if (loggedInFromGoogle || loggedInFromEmail) {
+    console.log("User logged In");
+    
+}
+    
