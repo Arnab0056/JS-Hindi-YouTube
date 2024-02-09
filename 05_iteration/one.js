ce49679657f7ea62eaa732e89c0e88f15e94b2d1
Arnab Dutta <137763219+Arnab0056@users.noreply.github.com>
2024-02-09 04:07:53 +0000

    change the save

--- 05_iteration/one.js
+++ 05_iteration/one.js
@@ -9,9 +9,36 @@
 //      console.log(element);
 //      }
 
-for (let i = 1; i <= 10; i++) {
-    for (let j = 1; j <= 10; j++) {
-        console.log(`Inner Loop value ${j} and outer loop value ${i}`);
-        }
+// for (let i = 1; i <= 10; i++) {
+//     for (let j = 1; j <= 10; j++) {
+//         // console.log(`Inner Loop value ${j} and outer loop value ${i}`);
+//         console.log(i + '*' + j + ' = ' + i*j );
+       
+//     }
     
+// }
+
+// let myArray = ["IronMan", "SuperMan", "SpiderMan"]
+
+// console.log(myArray.length)
+// for (let index = 0; index < myArray.length; index++) {
+//     const element = myArray[index];
+//     console.log(element);
+    
+// }
+
+// for (let i = 0; i < 20; i++) {
+//     if (i == 5 ) {
+//         console.log('Detected 5');
+//         break;
+//     };
+//     console.log(`Value of i is ${i}`);
+// }
+
+for (let i = 0; i <= 20; i++) {
+    if (i == 5 ) {
+        console.log('Detected 5');
+        continue;
+    };
+    console.log(`Value of i is ${i}`);
 }
