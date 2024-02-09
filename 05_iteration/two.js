377818a64fe38daff7becc22ac52a232bbf2b663
Arnab Dutta <137763219+Arnab0056@users.noreply.github.com>
2024-02-09 07:35:14 +0000

    Save the change

---
+++ 05_iteration/two.js
@@ -0,0 +1,27 @@
+// // let index = 0;
+// // while (index<= 10) {
+// //     console.log(`value of index is ${index}`);
+// //     index = index +2
+// // }
+// let myArray = ["superman", "Spiderman", "Batman"]
+// let arr = 0;
+// while (arr<myArray.length) {
+//     console.log(`value is ${myArray[arr]}`)
+//     arr = arr+1
+// }
+
+
+// let myArray1 = [4,6,7,8,10]
+// let index = 0
+// while (index<myArray1.length) {
+//     console.log(`my value is ${myArray1[index]}`);
+//     index = index+ 1;
+// }
+
+
+// DO While loop---------
+let score = 2
+do {
+    console.log(`score is ${score}`);
+    score++
+} while (score <= 11);
