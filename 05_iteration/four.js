74ee23f06c4b4a53c058155bdf9d63aec79a1d22
Arnab Dutta <137763219+Arnab0056@users.noreply.github.com>
2024-02-16 15:07:30 +0000

    change

---
+++ 05_iteration/four.js
@@ -0,0 +1,27 @@
+const myObject = {
+    js: 'javascript',
+    cpp: 'C++',
+    rb: "ruby",
+    swift: "swift by apple"
+}
+
+// for (const key in myObject) {
+//     console.log(key);
+// }
+// for (const key in myObject) {
+//     console.log(myObject[key]);
+// }
+
+//  
+
+// const programming = ["js", "rb", "py", "java", "cpp"]
+
+// for (const key in programming) {
+//     console.log(programming[key]);
+// }
+
+
+
+
+// ARRAY , MAP ----- FOR OF LOOP
+// ObJECT ----- FOR In LOOP
